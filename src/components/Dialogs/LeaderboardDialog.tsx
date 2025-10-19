9b415ff1c122ced90270ce999a4fd996ef3b1702
Karim248119 <karimoda66@gmail.com>
2025-10-19 21:09:05 +0300

    Refactor LeaderboardDialog to improve layout

--- src/components/Dialogs/LeaderboardDialog.tsx
+++ src/components/Dialogs/LeaderboardDialog.tsx
@@ -8,7 +8,6 @@ import {
 import IconButton from "../Buttons/IconButton";
 import { BiTrophy } from "react-icons/bi";
 import { X } from "lucide-react";
-import Header from "../Header";
 
 const LeaderboardDialog = () => {
   return (
@@ -16,7 +15,7 @@ const LeaderboardDialog = () => {
       <DialogTrigger asChild>
         <IconButton icon={<BiTrophy />} toltip="Leaderboard" />
       </DialogTrigger>
-      <DialogOverlay className="w-screen h-screen bg-white md:p-10 p-5">
+      <DialogOverlay className="w-screen h-screen overflow-y-auto bg-white md:p-10 p-5">
         <DialogClose asChild>
           <button className="absolute top-5 right-5 md:w-8 w-6 aspect-square flex-center rounded-lg hover:bg-teal-500 hover:text-white border border-teal-500 duration-300">
             <X className="w-4 md:w-6" />
@@ -36,7 +35,7 @@ const LeaderboardDialog = () => {
               أشوف اسمك في البورد هنا.
             </p>
           </div>
-          <div className="w-full flex-1 mx-auto md:p-5 p-3 md:rounded-2xl rounded-lg bg-teal-500/10 grid lg:grid-rows-5 md:grid-cols-2 md:gap-5 gap-2">
+          <div className="w-full flex-1 mx-auto md:p-5 p-3 md:rounded-2xl rounded-lg bg-teal-500/10 grid md:grid-rows-5 md:grid-cols-2 md:gap-5 gap-2">
             {Array.from({ length: 10 }).map((_, index) => (
               <div
                 key={index}
